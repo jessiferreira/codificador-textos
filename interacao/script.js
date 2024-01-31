867dcb30ef8648f45f73cc31ee615a7a40d31835
Jéssica <ferrsjessy@gmail.com>
2024-01-31 08:24:02 -0300

    Atualizações

--- interacao/script.js
+++ interacao/script.js
@@ -3,7 +3,7 @@ var outInput = document.querySelector("#output");
 
 function limparTexto() {
     document.getElementById('input-texto').value = '';
-    document.getElementById('output').innerHTML = '<img src="/imagens/Star.png" alt="Estrela">' +
+    document.getElementById('output').innerHTML = '<img src="/imagens/gato.png" alt="logo-gato">' +
         '<h2>Nenhuma mensagem encontrada!</h2>' +
         '<span>Digite um texto que você deseja criptografar ou descriptografar.</span>';
     document.querySelector('.btn-descriptografar').disabled = true;
@@ -14,10 +14,10 @@ function criptografar() {
     var texto = textInput.value;
 
     var resultCriptografar = texto.replace(/e/g, "enter").replace(/i/g, "imes").replace(/a/g, "ai")
-    .replace(/o/g, "ober").replace(/u/g, "ufat");
+        .replace(/o/g, "ober").replace(/u/g, "ufat");
 
-    document.getElementById('output').innerHTML = '<textarea readonly id="input-texto">' + resultCriptografar + 
-    '</textarea>' + '</br><button class="btn-copiar" id="copiar" onclick="copiar()">Copiar</button>'; 
+    document.getElementById('output').innerHTML = '<textarea readonly id="input-texto">' + resultCriptografar +
+        '</textarea>' + '</br><button class="btn-copiar" id="copiar" onclick="copiar()">Copiar</button>';
 
     document.querySelector('.btn-descriptografar').disabled = false;
     document.querySelector('.btn-limpar').disabled = false;
@@ -27,10 +27,10 @@ function descriptografar() {
     var texto = textInput.value;
 
     var resultDescriptografar = texto.replace(/enter/g, "e").replace(/imes/g, "i").replace(/ai/g, "ai")
-    .replace(/ober/g, "o").replace(/ufat/g, "u");
+        .replace(/ober/g, "o").replace(/ufat/g, "u");
 
-    document.getElementById('output').innerHTML = '<textarea readonly id="input-texto">' + resultDescriptografar + 
-    '</textarea>' + '</br><button class="btn-copiar" id="copiar" onclick="copiar()">Copiar</button>';
+    document.getElementById('output').innerHTML = '<textarea readonly id="input-texto">' + resultDescriptografar +
+        '</textarea>' + '</br><button class="btn-copiar" id="copiar" onclick="copiar()">Copiar</button>';
 
     document.querySelector('.btn-descriptografar').disabled = false;
     document.querySelector('.btn-limpar').disabled = false;
